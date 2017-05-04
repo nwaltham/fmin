2250d82fca653b5d70648ff92ea11d3ccffdee54
Nicholas Waltham <nw221@log1.net>
2017-05-04 12:45:13 +0200

    adding some debug info

--- src/bisect.js
+++ src/bisect.js
@@ -1,32 +1,36 @@
 /** finds the zeros of a function, given two starting points (which must
  * have opposite signs */
 export function bisect(f, a, b, parameters) {
-    parameters = parameters || {};
-    var maxIterations = parameters.maxIterations || 100,
-        tolerance = parameters.tolerance || 1e-10,
-        fA = f(a),
-        fB = f(b),
-        delta = b - a;
-
-    if (fA * fB > 0) {
-        throw "Initial bisect points must have opposite signs";
-    }
-
-    if (fA === 0) return a;
-    if (fB === 0) return b;
-
-    for (var i = 0; i < maxIterations; ++i) {
-        delta /= 2;
-        var mid = a + delta,
-            fMid = f(mid);
-
-        if (fMid * fA >= 0) {
-            a = mid;
-        }
-
-        if ((Math.abs(delta) < tolerance) || (fMid === 0)) {
-            return mid;
-        }
-    }
-    return a + delta;
+       parameters = parameters || {};
+        var maxIterations = parameters.maxIterations || 100,
+            tolerance = parameters.tolerance || 1e-10,
+            fA = f(a),
+            fB = f(b),
+            delta = b - a;
+            if (Math.abs(fA)<0.00001) { fA = 0}
+            if (Math.abs(fB)<0.00001) { fB = 0}
+// console.log(a+";"+b +" --- "+fA+";"+fB);
+        if (fA * fB > 0) {
+            console.log("Initial bisect points must have opposite signs");
+            console.log(`a = ${a}`);
+            console.log(`b = ${b}`);
+            console.log(`fA = ${fA}`);
+            console.log(`fB = ${fB}`);
+            console.log(`fA * fB = ${fA * fB}`);
+            throw "Initial bisect points must have opposite signs";
+        }
+        if (fA === 0) return a;
+        if (fB === 0) return b;
+        for (var i = 0; i < maxIterations; ++i) {
+            delta /= 2;
+            var mid = a + delta,
+                fMid = f(mid);
+            if (fMid * fA >= 0) {
+                a = mid;
+            }
+            if ((Math.abs(delta) < tolerance) || (fMid === 0)) {
+                return mid;
+            }
+        }
+        return a + delta;
 }
